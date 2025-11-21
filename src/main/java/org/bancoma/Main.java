4b731897d100c218b80831a46ad7a02d44d4ae40
AleTane04 <gaetanoalessioforino@gmail.com>
2025-11-21 10:20:25 +0100

    number of bytes that can be read without blocking edit

--- src/main/java/org/bancoma/Main.java
+++ src/main/java/org/bancoma/Main.java
@@ -1,17 +1,18 @@
 package org.bancoma;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.io.IOException;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Enter a character:");
+
+        // Reads a single byte from System.in
+        int data = System.in.read();
+        int available = System.in.available();
+
+        // Print the character and its ASCII value
+        System.out.println("You entered: " + (char) data);
+        System.out.println("ASCII Value: " + data);
+        System.out.println("Number of bytes that can be read without blocking: " + available);
     }
 }
