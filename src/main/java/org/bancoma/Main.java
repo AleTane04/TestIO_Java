433048d3a5dab6341ed4277d2ff1bfeaf7fcf50b
AleTane04 <gaetanoalessioforino@gmail.com>
2025-11-21 10:32:46 +0100

    ByteStream

--- src/main/java/org/bancoma/Main.java
+++ src/main/java/org/bancoma/Main.java
@@ -1,6 +1,6 @@
 package org.bancoma;
 
-import java.io.IOException;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -14,5 +14,31 @@ public class Main {
         System.out.println("You entered: " + (char) data);
         System.out.println("ASCII Value: " + data);
         System.out.println("Number of bytes that can be read without blocking: " + available);
+
+
+        /* ByteStream */
+
+        FileInputStream myInputFile = null;
+        FileOutputStream myOutputFile = null;
+        try
+        {
+            myInputFile = new FileInputStream("input.txt");
+            myOutputFile = new FileOutputStream("output.txt");
+
+            int temp;
+            while((temp = myInputFile.read()) != -1)
+            {
+                myOutputFile.write(temp);
+            }
+
+        }
+            finally
+        {
+            if(myInputFile != null)
+                myInputFile.close();
+            if(myOutputFile != null)
+                myOutputFile.close();
+        }
+
     }
 }
