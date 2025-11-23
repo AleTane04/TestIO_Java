bc6e2ccdaefa15b5c0459898fdc59858fcdf7162
AleTane04 <gaetanoalessioforino@gmail.com>
2025-11-23 10:28:28 +0100

    Un nuovo inizio

--- src/main/java/org/bancoma/Main.java
+++ src/main/java/org/bancoma/Main.java
@@ -2,45 +2,14 @@ package org.bancoma;
 
 import java.io.*;
 
-public class Main {
+public class Main
+{
     public static void main(String[] args) throws IOException
     {
-        System.out.println("Directory corrente: " + System.getProperty("user.dir"));
-        System.out.println("Enter a character:");
-
-        // Reads a single byte from System.in
-        int data = System.in.read();
-        int available = System.in.available();
-
-        // Print the character and its ASCII value
-        System.out.println("You entered: " + (char) data);
-        System.out.println("ASCII Value: " + data);
-        System.out.println("Number of bytes that can be read without blocking: " + available);
-
-
-        /* ByteStream */
-
-        FileInputStream myInputFile = null;
-        FileOutputStream myOutputFile = null;
-        try
-        {
-            myInputFile = new FileInputStream("input.txt");
-            myOutputFile = new FileOutputStream("output.txt");
-
-            int temp;
-            while((temp = myInputFile.read()) != -1)
-            {
-                myOutputFile.write(temp);
-            }
-
-        }
-            finally
-        {
-            if(myInputFile != null)
-                myInputFile.close();
-            if(myOutputFile != null)
-                myOutputFile.close();
-        }
-
+        int data;
+        System.out.println("Inserisci un carattere: ");
+        data = System.in.read();
+        System.out.println("Hai inserito il carattere: " + (char)data);
+        System.out.println("Il codice ASCII è: " + data);
     }
 }
